bd1f28ad596841e303c83c0476b853ae565e6cfa
narcizo <narcizo.gabriel2@gmail.com>
2023-08-07 17:36:30 -0300

    first commit

---
+++ src/main/java/com/narcizo/TesteIrrah/TesteIrrahApplication.java
@@ -0,0 +1,13 @@
+package com.narcizo.TesteIrrah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteIrrahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteIrrahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/narcizo/TesteIrrah/TesteIrrahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narcizo.TesteIrrah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteIrrahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
