b31a79b557600effaebc0afc3969d25e32adf9ae
narcizo <narcizo.gabriel2@gmail.com>
2023-08-08 01:10:25 -0300

    Ajustes para o Docker

---
+++ README.md
@@ -0,0 +1,9 @@
+# Teste-GrupoIrrah
+
+Java 20
+Spring-boot 3.1.2
+
+
+# Docker Hub repos
+narcizo/teste_irrah_backend_img
+narcizo/teste-irrah-postgre-img
--- src/main/java/com/narcizo/TesteIrrah/TesteIrrahApplication.java
+++ src/main/java/com/narcizo/TesteIrrah/TesteIrrahApplication.java
@@ -2,9 +2,17 @@ package com.narcizo.TesteIrrah;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TesteIrrahApplication {
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return "docker funcionando";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TesteIrrahApplication.class, args);
