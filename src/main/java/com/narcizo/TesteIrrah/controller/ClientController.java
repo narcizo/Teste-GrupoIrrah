dda98d55cf37ab2fda2a96e77a27bb491cb49579
narcizo <narcizo.gabriel2@gmail.com>
2023-08-08 23:48:09 -0300

    Client refatorado

--- src/main/java/com/narcizo/TesteIrrah/controller/ClientController.java
+++ src/main/java/com/narcizo/TesteIrrah/controller/ClientController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,47 +20,32 @@ public class ClientController {
 
     @GetMapping
     public List<Client> listAllClients () {
-        System.out.println("List clients");
-        return repository.findAll();
+        return service.getClientList();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<Client>> getClientById(@PathVariable Long id){
-        Optional<Client> client = repository.findById(id);
-        System.out.println("List single client");
+    public ResponseEntity<Client> getClientById(@PathVariable Long id){
+        Client client = service.getCLient(id);
 
         return ResponseEntity.ok(client);
     }
 
     @PostMapping
-    public void saveClient (@RequestBody Client client) {
-        repository.save(client);
+    public ResponseEntity<Client> saveClient (@RequestBody Client client) {
+        Client created = service.createClient(client);
         System.out.println("Save client");
+        return ResponseEntity.ok(created);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Optional<Client>> updateClient(@PathVariable Long id, @RequestBody Client updatedClient){
-        Optional<Client> client = repository.findById(id);
-
-        client.ifPresent(c -> c.setName(updatedClient.getName()));
-        client.ifPresent(c -> c.setEmail(updatedClient.getEmail()));
-        client.ifPresent(c -> c.setPhone(updatedClient.getPhone()));
-        client.ifPresent(c -> c.setCpf(updatedClient.getCpf()));
-        client.ifPresent(c -> c.setCnpj(updatedClient.getCnpj()));
-        client.ifPresent(c -> c.setCompanyName(updatedClient.getCompanyName()));
-
-        client.ifPresent(c -> repository.save(c));
-
-        System.out.println("Update client");
-        return ResponseEntity.ok(client);
+    public ResponseEntity<Client> updateClient(@PathVariable Long id, @RequestBody Client updatedClient){
+        Client updated = service.updateClient(id, updatedClient);
+        return ResponseEntity.ok(updated);
     }
 
     @DeleteMapping("/{id}")
     public void deleteClient(@PathVariable Long id){
-        Optional<Client> client = repository.findById(id);
-
-        client.ifPresent(c -> repository.delete(c));
-        System.out.println("Delete client");
+        service.deleteClient(id);
     }
 
 }
--- src/main/java/com/narcizo/TesteIrrah/service/ClientService.java
+++ src/main/java/com/narcizo/TesteIrrah/service/ClientService.java
@@ -1,11 +1,48 @@
 package com.narcizo.TesteIrrah.service;
 
+import com.narcizo.TesteIrrah.entity.Client;
 import com.narcizo.TesteIrrah.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClientService {
     @Autowired
     ClientRepository repository;
+
+    public Client getCLient(Long clientId){
+        return repository.findById(clientId).
+                orElseThrow(() -> new ResourceNotFoundException("Client not found with id: " + clientId));
+    }
+
+    public List<Client> getClientList(){
+        return repository.findAll();
+    }
+
+    public Client createClient(Client client){
+        return repository.save(client);
+    }
+
+    public Client updateClient(Long clientId, Client updatedClient) {
+        Client existingClient = repository.findById(clientId)
+                .orElseThrow(() -> new ResourceNotFoundException("Client not found with id: " + clientId));
+
+        existingClient.setName(updatedClient.getName());
+        existingClient.setEmail(updatedClient.getEmail());
+        existingClient.setPhone(updatedClient.getPhone());
+        existingClient.setCpf(updatedClient.getCpf());
+        existingClient.setCnpj(updatedClient.getCnpj());
+        existingClient.setCompanyName(updatedClient.getCompanyName());
+
+        return repository.save(existingClient);
+    }
+
+    public void deleteClient(Long clientId){
+        Client client = this.getCLient(clientId);
+
+        repository.delete(client);
+    }
 }
